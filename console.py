3342a6fb74d2fb1697aadb5d9631adeb6ee573e5
JaiberS <758@holbertonschool.com>
2019-06-29 19:40:36 +0000

    [Feature] Basic console

---
+++ console.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    "Command processor for HBNB\n"
+
+    def emptyline(self):
+        return
+
+    def do_EOF(self, line):
+        "Exit\n"
+        return True
+
+    def do_quit(self, line):
+        "Quit command to exit the program\n"
+        return True
+
+
+if __name__ == '__main__':
+    HBNBCommand.prompt = "(hbnb) "
+    HBNBCommand().cmdloop()
