e711d0df1a6b7051ad4bd04c47418bfae41d65a2
JaiberS <758@holbertonschool.com>
2019-07-01 20:23:49 +0000

    [Feature] Create command, new instance and save to json

--- console.py
+++ console.py
@@ -1,10 +1,22 @@
 #!/usr/bin/python3
 """ module to create the console """
 import cmd
+from models.base_model import BaseModel
 
 
 class HBNBCommand(cmd.Cmd):
     "Command processor for HBNB\n"
+
+    def do_create(self, args):
+        if len(args) == 0:
+            print("** class name missing **")
+            return
+        if args != "BaseModel":
+            print("** class doesn't exist **")
+            return
+        my_model = BaseModel()
+        my_model.save()
+        print(my_model.id)
 
     def emptyline(self):
         return
