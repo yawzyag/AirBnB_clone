71c7a357bd04c5b5b7895d1e0d410e19ecb1edcc
yawzyag <yesid.yag@gmail.com>
2019-07-01 19:53:40 +0000

    [Feature] Adding the class User

--- models/user.py
+++ models/user.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+""" module to add the user into the project """
 from models.base_model import BaseModel
 
 
 class User(BaseModel):
+    """ Create user class """
     email = ""
     password = ""
     first_name = ""
