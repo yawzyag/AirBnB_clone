06ec2568b576247451e3a5a779fe17f60cf9ccee
yawzyag <yesid.yag@gmail.com>
2019-07-02 15:11:54 +0000

    [Feature] adding more classes

---
+++ models/state.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" module to add the user into the project """
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """ Create state class """
+    name = ""
